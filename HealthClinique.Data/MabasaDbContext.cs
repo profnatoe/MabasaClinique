17e6f90e62e54ad96f3fc11e0bb34b204559837d
nando sitoe <sitoenando@outlook.com>
2020-09-11 23:50:23 +0200

    Added A DbContext class

---
+++ HealthClinique.Data/MabasaDbContext.cs
@@ -0,0 +1,19 @@
+﻿using HealthClinique.Data.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HealthClinique.Data
+{
+    public class MabasaDbContext  : IdentityDbContext
+    {
+        public MabasaDbContext(DbContextOptions<MabasaDbContext> options) : base(options) { }
+
+        public DbSet<Patient> Patients { get; set; }
+        public DbSet<PatientAddress> PatientAddresses { get; set; }
+        public DbSet<PatientTests> PatientTests { get; set; }
+        public DbSet<Appointment> Appointment { get; set; }
+    }
+}
