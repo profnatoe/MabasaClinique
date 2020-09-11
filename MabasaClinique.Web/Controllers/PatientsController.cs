75d5f9f76bc4da9035c466759deecc2dd085f861
nando sitoe <sitoenando@outlook.com>
2020-09-12 01:20:38 +0200

    Added PatientController and updated launchSettings

---
+++ MabasaClinique.Web/Controllers/PatientsController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HealthClinique.Service.Patients;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MabasaClinique.Web.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/")]
+    public class PatientsController : ControllerBase
+    {
+        private readonly IPatientService _patients;
+
+        public PatientsController(IPatientService patientService)
+        {
+            _patients = patientService;
+        }
+        [HttpGet]
+        public ActionResult GetAllPatients()
+        {
+            var results =  _patients.GetAllPatient();
+
+            return Ok(results);
+        }
+    }
+}
