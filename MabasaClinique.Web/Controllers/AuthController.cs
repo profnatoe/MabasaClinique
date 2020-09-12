8872a9641029446cd325d56fa63c3dbf59a7d310
profnatoe <sitoenando@outlook.com>
2020-09-12 16:54:07 +0200

    Added AuthController.cs For Login and Registration of a Patient

---
+++ MabasaClinique.Web/Controllers/AuthController.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading.Tasks;
+using HealthClinique.Data.Models;
+using HealthClinique.Service.User;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MabasaClinique.Web.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public AuthController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpPost("Register")]
+        public async Task<IActionResult> RegisterPatient([FromBody] RegisterUser user)
+        {
+            if (ModelState.IsValid)
+                return Ok(await _userService.RegisterPatient(user));
+            return BadRequest();
+        }
+
+        [HttpPost("Login")]
+        public async Task<IActionResult> LoginPatient([FromBody] LoginUser user)
+        {
+            if (ModelState.IsValid)
+                return Ok(await _userService.LoginPatient(user));
+            return BadRequest();
+        }
+    }
+}
